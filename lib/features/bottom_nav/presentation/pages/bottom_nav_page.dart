6c7e6fded147a24202995a78ede714006331587d
cahyo40 <mdcnugroho@gmail.com>
2025-12-03 09:20:11 +0700

    feat: Migrate to hooks_riverpod and add a responsive drawer with yo_ui components, conditionally replacing the bottom navigation bar based on orientation.

--- lib/features/bottom_nav/presentation/pages/bottom_nav_page.dart
+++ lib/features/bottom_nav/presentation/pages/bottom_nav_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yo_sales/core/const/role.dart';
 import 'package:yo_sales/features/cashier/presentation/pages/cashier_page.dart';
 import 'package:yo_sales/features/history/presentation/pages/history_page.dart';
@@ -9,10 +10,11 @@ import 'package:yo_sales/features/product/presentation/pages/product_page.dart';
 import 'package:yo_sales/features/profile/presentation/pages/profile_page.dart';
 import 'package:yo_sales/features/report/presentation/pages/report_page.dart';
 import 'package:yo_sales/l10n/app_localizations.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 import '../providers/bottom_nav_provider.dart';
 
-class BottomNavPage extends ConsumerWidget {
+class BottomNavPage extends HookConsumerWidget {
   const BottomNavPage({super.key});
 
   @override
@@ -21,6 +23,7 @@ class BottomNavPage extends ConsumerWidget {
     final notifier = ref.read(bottomNavNotifierProvider.notifier);
     final l10n = AppLocalizations.of(context)!;
     final userRole = Role.cashier;
+    final toggleDrawer = useState(false);
 
     List<BottomNavigationBarItem> buildBottomNavigationBar() {
       List<BottomNavigationBarItem> navItems;
@@ -93,6 +96,125 @@ class BottomNavPage extends ConsumerWidget {
       return navItems;
     }
 
+    Widget buildDrawer(BuildContext context) {
+      final List<Widget> children;
+
+      Widget cardMenu({
+        required String title,
+        required int index,
+        required IconData icon,
+      }) {
+        void handleTap(int i) {
+          switch (userRole) {
+            case Role.cashier:
+              notifier.changePageCashier(i);
+              break;
+            case Role.admin:
+              notifier.changePageAdmin(i);
+              break;
+            case Role.owner:
+              notifier.changePageOwner(i);
+              break;
+          }
+        }
+
+        final i = ref.watch(bottomNavNotifierProvider).value ?? 0;
+
+        return YoAnimatedCrossSwitch(
+          condition: toggleDrawer.value == true,
+          secondChild: Center(
+            child: Padding(
+              padding: YoPadding.bottom16,
+              child: YoButtonIcon.custom(
+                tooltip: title,
+                size: YoIconButtonSize.large,
+                backgroundColor: i == index
+                    ? context.primaryColor
+                    : Colors.transparent,
+                icon: Icon(icon),
+                onPressed: () => handleTap(index),
+              ),
+            ),
+          ),
+          firstChild: YoListTile(
+            leading: Icon(icon),
+            title: title,
+            selected: i == index,
+            onTap: () => handleTap(index),
+          ),
+        );
+      }
+
+      if (userRole == Role.cashier) {
+        children = [
+          cardMenu(title: l10n.cashier, index: 0, icon: Icons.point_of_sale),
+
+          cardMenu(title: l10n.product, index: 1, icon: Icons.inventory_2),
+          cardMenu(title: l10n.history, index: 2, icon: Icons.history),
+          cardMenu(title: l10n.profile, index: 3, icon: Icons.person),
+        ];
+      } else if (userRole == Role.admin) {
+        children = [
+          cardMenu(title: l10n.home, index: 0, icon: Icons.dashboard),
+          cardMenu(title: l10n.cashier, index: 1, icon: Icons.point_of_sale),
+          cardMenu(title: l10n.product, index: 2, icon: Icons.inventory_2),
+          cardMenu(title: l10n.report, index: 3, icon: Icons.bar_chart),
+          cardMenu(title: l10n.profile, index: 4, icon: Icons.person),
+        ];
+      } else {
+        // Owner
+        children = [
+          cardMenu(title: l10n.home, index: 0, icon: Icons.dashboard),
+          cardMenu(title: l10n.report, index: 1, icon: Icons.bar_chart),
+          cardMenu(title: l10n.outlet, index: 2, icon: Icons.store),
+          cardMenu(title: l10n.product, index: 3, icon: Icons.inventory_2),
+          cardMenu(title: l10n.profile, index: 4, icon: Icons.person),
+        ];
+      }
+
+      return Drawer(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: context.primaryColor),
+              child: Row(
+                mainAxisAlignment: toggleDrawer.value ? .spaceBetween : .center,
+                children: [
+                  YoAnimatedCrossSwitch(
+                    condition: toggleDrawer.value,
+                    firstChild: Text(
+                      userRole.name(context),
+                      style: context.yoTitleLarge,
+                    ),
+                    secondChild: SizedBox(),
+                  ),
+                  YoButtonIcon.ghost(
+                    icon: YoAnimatedCrossSwitch(
+                      condition: !toggleDrawer.value,
+                      firstChild: Icon(
+                        Icons.arrow_circle_right,
+                        color: context.onPrimaryColor,
+                      ),
+                      secondChild: Icon(
+                        Icons.arrow_circle_left,
+                        color: context.onPrimaryColor,
+                      ),
+                    ),
+                    onPressed: () {
+                      toggleDrawer.value = !toggleDrawer.value;
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ...children,
+          ],
+        ),
+      );
+    }
+
     void onBottomNavTapped(int index) {
       switch (userRole) {
         case Role.cashier:
@@ -108,11 +230,14 @@ class BottomNavPage extends ConsumerWidget {
     }
 
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onBottomNavTapped,
-        items: buildBottomNavigationBar(),
-        currentIndex: asyncState.value ?? 0,
-      ),
+      bottomNavigationBar: context.yoIsLandscape
+          ? SizedBox.shrink()
+          : BottomNavigationBar(
+              onTap: onBottomNavTapped,
+              items: buildBottomNavigationBar(),
+              currentIndex: asyncState.value ?? 0,
+              selectedItemColor: context.primaryColor,
+            ),
       body: Consumer(
         builder: (_, ref, _) {
           final index = ref.watch(bottomNavNotifierProvider).value ?? 0;
@@ -145,7 +270,21 @@ class BottomNavPage extends ConsumerWidget {
           }
 
           return SafeArea(
-            child: IndexedStack(index: index, children: pages()),
+            child: context.yoIsLandscape
+                ? Row(
+                    children: [
+                      AnimatedContainer(
+                        width: toggleDrawer.value ? 275 : 90,
+                        duration: const Duration(milliseconds: 300),
+                        child: buildDrawer(context),
+                      ),
+                      Expanded(
+                        flex: 8,
+                        child: IndexedStack(index: index, children: pages()),
+                      ),
+                    ],
+                  )
+                : IndexedStack(index: index, children: pages()),
           );
         },
       ),
